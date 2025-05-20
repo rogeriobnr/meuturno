76362475599efdedea434c9a073b9b4b90eefe64
rogeriobnr <rogeriobnr@gmail.com>
2025-05-20 17:39:15 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,29 @@
-const CACHE_NAME = 'meu-turno-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'meuturno-cache-v1';
+const URLS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
+  '/style.css',
+  '/meuturno.js',
   '/icon-192.png',
   '/icon-512.png'
 ];
 
-// Instalando o Service Worker
+// Instalando
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-// Ativando e limpando caches antigos
+// Ativando
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keyList => 
-      Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }))
-    )
-  );
+  event.waitUntil(clients.claim());
 });
 
-// Interceptando requisições
+// Interceptando requests
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
