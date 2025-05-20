bdcbf09c2362fb93c1d0feecc0e392bb31b215b4
rogeriobnr <rogeriobnr@gmail.com>
2025-05-20 15:58:08 -0300

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'meu-turno-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Instalando o Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Ativando e limpando caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => 
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+});
+
+// Interceptando requisições
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
