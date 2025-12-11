c9c23b955c7e0052eb283d82e5d9da4b0ebf47b8
rogeriobnr <rogeriobnr@gmail.com>
2025-12-11 06:59:58 -0300

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 // 1. VERSÃO E CONFIGURAÇÃO
 // Incremento a versão para v51 para forçar o ciclo de update no navegador
-const CACHE_NAME = 'meuturno-cache-v56';
+const CACHE_NAME = 'meuturno-cache-v57';
 
 // Tempo limite para esperar pela rede em navegações (3 segundos)
 const NETWORK_TIMEOUT_MS = 3000;
